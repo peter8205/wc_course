9e6b88d9f934b64b950d351c83e54b93ebcc0207
Peter Vecera <vecerapeto@gmail.com>
2021-05-23 20:25:58 +0200

    index wp_loop pridane dva clanky

--- index.php
+++ index.php
@@ -38,7 +38,24 @@
 				</section>
 				<section class="lab-blog">
 					<div class="container">
-						<div class="row">News</div>
+						<div class="row">
+							<?php 
+								if(have_posts()):
+									while( have_posts() ): the_post();
+									?>
+										<article>
+											<h2> <?php  the_title(); ?> </h2>
+											<div> <?php  the_content(); ?> </div>
+										</article>
+									<?php	
+									endwhile;
+								else:
+									?>
+											<p>Nothing to display.</p>
+									<?php 
+								endif;
+							 ?>
+						</div>
 					</div>
 				</section>
 			</main>
