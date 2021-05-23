671cbdd9aa26d7a3f3d9f0abd45261a54e678f37
Peter Vecera <vecerapeto@gmail.com>
2021-05-23 19:24:04 +0200

    header footer index lekcia 10

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package PetoBear
+ */
+?>
+<footer>
+			<section class="footer-widgets">Footer Widget</section>
+			<section class="copyright">Copyright</section>
+		</footer>
+	</div>
+	
+</body>
+</html>
