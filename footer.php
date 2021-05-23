8ad23ffd05f4e482bf5fbb7a5269af1a173f1ccd
Peter Vecera <vecerapeto@gmail.com>
2021-05-23 20:22:37 +0200

    zmeny index footer function style.css footer menu lek 17

--- footer.php
+++ footer.php
@@ -7,9 +7,25 @@
  * @package PetoBear
  */
 ?>
-		<footer>
-			<section class="footer-widgets">Footer Widget</section>
-			<section class="copyright">Copyright</section>
+	<section class="footer-widgets">
+				<div class="container">
+					<div class="row">Footer Widgets</div>
+				</div>
+			</section>
+			<section class="copyright">
+				<div class="container">
+					<div class="row">
+						<div class="copy-right-text col-12 col-md-6">Copyright</div>
+						<nav class="footer-menu col-12 cl-md6 text-left text-md-right">
+								<?php wp_nav_menu(
+									array(
+										'thme_location' => 'petoBear_lab_footer_menu'
+									)
+								); ?>
+						</nav>
+					</div>
+				</div>
+			</section>
 		</footer>
 	</div>
 	<?php wp_footer() ?>
