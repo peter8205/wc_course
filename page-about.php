199f7c0646678e1787e12651188620ddb9235124
Peter Vecera <vecerapeto@gmail.com>
2021-05-23 20:32:26 +0200

    pridane sablony page-about.php a page-kontakt.php lek 19

---
+++ page-about.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package petoBear-lab
+ */
+?>
+
+<?php get_header() ?>
+		<div class="content-area">
+			<main>
+				<h1>Extra stranka pre ABOUT </h1>
+				<p>Tak ze toto je stranka o mne</p>
+			</main>
+		</div>
+<?php get_footer() ?>
