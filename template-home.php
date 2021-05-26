8b0339601cf56204a4f176dc24e6fdfe28757c38
Peter Vecera <vecerapeto@gmail.com>
2021-05-26 18:39:04 +0200

    template-home.php lek 46-47 Home šablona Home Page

--- template-home.php
+++ template-home.php
@@ -6,21 +6,38 @@ get_header();
 ?>
 		<div class="content-area">
 			<main>
-				<section class="slider">
+		<section class="slider">
 					<div class="flexslider">
 					  <ul class="slides">
+						<?php  
+						// Getting data from Customizer to display the Slider section
+						for ($i=1; $i < 4; $i++) : 
+							$slider_page[$i] 				= get_theme_mod( 'set_slider_page' . $i );
+							$slider_button_text[$i]			= get_theme_mod( 'set_slider_button_text' . $i ); 
+							$slider_button_url[$i]			= get_theme_mod( 'set_slider_button_url' . $i );
+						endfor;
+
+						$args = array(
+							'post_type'			=> 'page',
+							'posts_per_page'	=> 3,
+							'post__in'			=> $slider_page,
+							'orderby'			=> 'post__in',
+						);
+
+						$slider_loop = new WP_Query( $args );
+
+						if( $slider_loop->have_posts() ):
+							while( $slider_loop->have_posts() ):
+								$slider_loop->the_post();
+						?>
 					    <li>
-					      <img src="slide1.jpg" />
+					      <?php the_post_thumbnail( 'petoBear-lab-slider', array( 'class' => 'img-fluid' ) ); ?>
 					    </li>
-					    <li>
-					      <img src="slide2.jpg" />
-					    </li>
-					    <li>
-					      <img src="slide3.jpg" />
-					    </li>
-					    <li>
-					      <img src="slide4.jpg" />
-					    </li>
+						<?php 
+						endwhile;
+						wp_reset_postdata();
+						endif;
+						?>
 					  </ul>
 					</div>
 				</section>
@@ -62,6 +79,7 @@ get_header();
 						</div>
 					</div>
 				</section>
+
 			</main>
 		</div>
 <?php get_footer(); ?>
