5428d8951bb07fe049a85d06214f8e85e7d651c4
Peter Vecera <vecerapeto@gmail.com>
2021-05-26 18:32:32 +0200

    customizer lek 45

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,7 +1,7 @@
 <?php 
 
 /**
- * petoBear Lab Theme Customizer
+ * Fany Lab Theme Customizer
  *
  * @package petoBear Lab
  */
@@ -35,5 +35,190 @@ function petoBear_lab_customizer( $wp_customize ){
 				)
 			);
 
+	/*--------------------------------------------------------------------------------*/
+	// Slider Section
+
+	$wp_customize->add_section(
+		'sec_slider', array(
+			'title'			=> 'Slider Settings',
+			'description'	=> 'Slider Section'
+		)
+	);	
+
+			// Field 1 - Slider Page Number 1
+
+			$wp_customize->add_setting(
+				'set_slider_page1', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'absint'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_slider_page1', array(
+					'label'			=> 'Set slider page 1',
+					'description'	=> 'Set slider page 1',
+					'section'		=> 'sec_slider',
+					'type'			=> 'dropdown-pages'
+				)
+			);			
+
+			// Field 2 - Slider Button Text Number 1
+
+			$wp_customize->add_setting(
+				'set_slider_button_text1', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'sanitize_text_field'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_slider_button_text1', array(
+					'label'			=> 'Button Text for Page 1',
+					'description'	=> 'Button Text for Page 1',
+					'section'		=> 'sec_slider',
+					'type'			=> 'text'
+				)
+			);
+
+			// Field 3 - Slider Button URL Number 1
+
+			$wp_customize->add_setting(
+				'set_slider_button_url1', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'esc_url_raw'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_slider_button_url1', array(
+					'label'			=> 'URL for Page 1',
+					'description'	=> 'URL for Page 1',
+					'section'		=> 'sec_slider',
+					'type'			=> 'url'
+				)
+			);	
+
+			/*---------------------------------------*/
+
+			// Field 4 - Slider Page Number 2
+
+			$wp_customize->add_setting(
+				'set_slider_page2', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'absint'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_slider_page2', array(
+					'label'			=> 'Set slider page 2',
+					'description'	=> 'Set slider page 2',
+					'section'		=> 'sec_slider',
+					'type'			=> 'dropdown-pages'
+				)
+			);			
+
+			// Field 5 - Slider Button Text Number 2
+
+			$wp_customize->add_setting(
+				'set_slider_button_text2', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'sanitize_text_field'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_slider_button_text2', array(
+					'label'			=> 'Button Text for Page 2',
+					'description'	=> 'Button Text for Page 2',
+					'section'		=> 'sec_slider',
+					'type'			=> 'text'
+				)
+			);
+
+			// Field 6 - Slider Button URL Number 2
+
+			$wp_customize->add_setting(
+				'set_slider_button_url2', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'esc_url_raw'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_slider_button_url2', array(
+					'label'			=> 'URL for Page 2',
+					'description'	=> 'URL for Page 2',
+					'section'		=> 'sec_slider',
+					'type'			=> 'url'
+				)
+			);			
+
+			/*---------------------------------------*/
+
+			// Field 7 - Slider Page Number 3
+
+			$wp_customize->add_setting(
+				'set_slider_page3', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'absint'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_slider_page3', array(
+					'label'			=> 'Set slider page 3',
+					'description'	=> 'Set slider page 3',
+					'section'		=> 'sec_slider',
+					'type'			=> 'dropdown-pages'
+				)
+			);			
+
+			// Field 8 - Slider Button Text Number 3
+
+			$wp_customize->add_setting(
+				'set_slider_button_text3', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'sanitize_text_field'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_slider_button_text3', array(
+					'label'			=> 'Button Text for Page 3',
+					'description'	=> 'Button Text for Page 3',
+					'section'		=> 'sec_slider',
+					'type'			=> 'text'
+				)
+			);
+
+			// Field 9 - Slider Button URL Number 3
+
+			$wp_customize->add_setting(
+				'set_slider_button_url3', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'esc_url_raw'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_slider_button_url3', array(
+					'label'			=> 'URL for Page 3',
+					'description'	=> 'URL for Page 3',
+					'section'		=> 'sec_slider',
+					'type'			=> 'url'
+				)
+			);
+
 }
-add_action( 'customize_register', 'petoBear_lab_customizer' );
+add_action( 'customize_register', 'petoBear_lab_customizer' ); 
