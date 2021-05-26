dbdae410047f66809369bf2064937ca8fe52f980
Peter Vecera <vecerapeto@gmail.com>
2021-05-26 18:16:49 +0200

    functions header style cart.svg lek 38

--- functions.php
+++ functions.php
@@ -9,6 +9,23 @@
 
 require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
 
+/**
+ * Show cart contents / total Ajax
+ */
+add_filter( 'woocommerce_add_to_cart_fragments', 'petoBear_lab_woocommerce_header_add_to_cart_fragment' );
+
+function petoBear_lab_woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+	?>
+	<span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+	<?php
+	$fragments['span.items'] = ob_get_clean();
+	return $fragments;
+}
+
 
 function petoBear_lab_scripts() {
 	wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array('jquery'), '5.0.0', true );
--- header.php
+++ header.php
@@ -32,7 +32,12 @@
 						<div class="brand col-md-3 col-12 col-lg-2 text-center text-md-left">Logo</div>
 						<div class="second-column col-md-9 col-12 col-lg-10">
 							<div class="row">
-								<div class="acount col-12">Account</div>
+								<div class="acount col-12">
+									<div class="cart text-right">
+										<a href="<?php echo wc_get_cart_url(); ?>"><span class="cart-icon"></span></a>
+										<span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+									</div>
+								</div>
 								<div class="col-12">
 									<nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
 										<!-- Brand and toggle get grouped for better mobile display -->
